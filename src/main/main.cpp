ac33b1409faab4c9807121a674658e492c1ef470
Mateus Cunha <mateuscunhazp@gmail.com>
2026-05-14 14:42:16 -0300

    começando a fazer o f5: validação das entradas

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,10 +2,54 @@
 
 using namespace std;
 
+bool validarBase(int base) {
+	bool valida = false;
+	
+	if ((base == 2) or (base == 8) or (base == 10) or (base == 16))
+		valida = true;
+	
+	if (!valida) {
+		cout << "Esta base não é válida!" << endl;
+	}
+	
+	return valida;
+}
+
+bool validarNumero (string numero, int base) {
+	bool valido = true;
+	if (base == 2) {
+		for (unsigned i = 0; i < numero.size(); i++) 
+			if ((numero[i] != '0') and (numero[i] != '1'))
+				valido = false;
+	}
+	
+	return valido;
+}
+
+
 int main() {
+	cout << "--- CONVERSOR UNIVERSAL DE SISTEMAS DE NUMERAÇÃO ---" << endl << endl;
+	cout << "Bases válidas para conversão: " << endl;
+	cout << "	- BINÁRIO     == 2" << endl;
+	cout << "	- OCTAL       == 8" << endl;
+	cout << "	- DECIMAL     == 10" << endl;
+	cout << "	- HEXADECIMAL == 16" << endl << endl;
 
-
-
+	cout << "Digite a base do número a ser convertido, seguido do número: ";
+	string numero;
+	int base;
+	cin >> base >> numero;
+	
+	while (!validarBase(base)) {
+		cout << "Digite uma base válida: ";
+		cin >> base;
+	}
+	
+	while (!validarNumero(numero, base)) {
+		cout << "Digite um número válido para sua base (" << base << "): ";
+		cin >> numero;
+	}
+	
 
 	return 0;
 }
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -2,10 +2,56 @@
 
 using namespace std;
 
+bool validarBase (int base) {
+	bool valida = false;
+	
+	if ((base == 2) or (base == 8) or (base == 10) or (base == 16))
+		valida = true;
+	
+	if (!valida) {
+		cout << "ERRO: Esta base não é válida!" << endl;
+	}
+	
+	
+	
+	return valida;
+}
+
+bool validarNumero (string numero, int base) {
+	bool valido = true;
+	if (base == 2) {
+		for (unsigned i = 0; i < numero.size(); i++) 
+			if ((numero[i] != '0') and (numero[i] != '1'))
+				valido = false;
+	} else if (base == 8) {
+		//falta fazer
+	} else if (base == 10) {
+		//falta fazer
+	} else if (base == 16) {
+		//falta fazer
+	}
+	
+	return valido;
+}
+
 int main() {
-
-
-
+	int base;
+	cout << "Digite a base: ";
+	cin >> base;
+	
+	while (!validarBase(base)) {
+		cout << "Digite a base: ";
+		cin >> base;
+	}
+	
+	string numero;
+	cout << "Digite o numero: ";
+	cin >> numero;
+	
+	while (!validarNumero(numero, base)) {
+		cout << "Digite um número válido para essa base (" << base << "): ";
+		cin >> numero;
+	}
 
 	return 0;
 }
